f95eda43fb0b219c5d586afcfe668f05ed30d526
Stanislaw <stanislawstachowski112@gmail.com>
2024-05-16 13:48:42 +0200

    Removed Creature field from DamageApplier, changed decorate methods to setters.

--- eco-battle-converter/src/test/java/pl/psi/hero/BattleSkillsTest.java
+++ eco-battle-converter/src/test/java/pl/psi/hero/BattleSkillsTest.java
@@ -5,6 +5,9 @@ import pl.psi.EconomyHero;
 import pl.psi.converter.EcoBattleConverter;
 import pl.psi.creatures.*;
 import skills.ArcherySkill;
+import skills.ArmorerSkill;
+import skills.OffenseSkill;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +17,6 @@ public class BattleSkillsTest {
 
     @Test
     void ArcherySkillTest(){
-        //given
         EconomyHero hero = new EconomyHero("name");
         hero.addSkill(new ArcherySkill(2));
         final EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
@@ -31,4 +33,30 @@ public class BattleSkillsTest {
         assertFalse(archeryCalculatorDecoratorClass.isInstance(calculator2));
     }
 
+    @Test
+    void armorerSkillTest() {
+        final EconomyHero ecoHero = new EconomyHero("name");
+        final EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
+        ecoHero.addCreature(factory.create(false, 1));
+        ecoHero.addCreature(factory.create(false, 2));
+        ecoHero.addSkill(new ArmorerSkill(1));
+        final List<Creature> convertedCreatures = EcoBattleConverter.convert(ecoHero)
+                .getCreatures();
+        //Czy da sie w jakis fajny sposob posprawdzac pola tych obiektow, np czy level tego damageAppliera sie zgadza
+        assertTrue(convertedCreatures.get(0).getDamageApplier() instanceof ArmoredDamageApplierDecorator);
+        assertTrue(convertedCreatures.get(1).getDamageApplier() instanceof ArmoredDamageApplierDecorator);
+    }
+
+    @Test
+    void offenseSkillTest() {
+        final EconomyHero ecoHero = new EconomyHero("name");
+        final EconomyNecropolisFactory factory = new EconomyNecropolisFactory();
+        ecoHero.addCreature(factory.create(false, 1));
+        ecoHero.addCreature(factory.create(false, 2));
+        ecoHero.addSkill(new OffenseSkill(1));
+        final List<Creature> convertedCreatures = EcoBattleConverter.convert(ecoHero)
+                .getCreatures();
+        assertTrue(convertedCreatures.get(0).getCalculator() instanceof OffenseCalculatorDecorator);
+        assertTrue(convertedCreatures.get(1).getCalculator() instanceof OffenseCalculatorDecorator);
+    }
 }
