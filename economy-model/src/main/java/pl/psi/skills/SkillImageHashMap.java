50330ec754b9b83cd93cbeff1b4e4c8a12168352
Tymoteusz Strojny <tymek.strojny@gmail.com>
2024-06-28 16:37:45 +0200

    added ballitic and resource images.

--- economy-model/src/main/java/pl/psi/skills/SkillImageHashMap.java
+++ economy-model/src/main/java/pl/psi/skills/SkillImageHashMap.java
@@ -13,6 +13,7 @@ public class SkillImageHashMap {
         skillImageMap.put(SkillEnum.ARMORER, "creatures/Armorer.png");
         skillImageMap.put(SkillEnum.OFFENSE, "creatures/Offense.png");
         skillImageMap.put(SkillEnum.LEADERSHIP, "creatures/Leadership.png");
+        skillImageMap.put(SkillEnum.BALLISTICS, "creatures/Ballistic.png");
     }
     public String get(SkillEnum askillEnum) {
         return skillImageMap.get(askillEnum);
