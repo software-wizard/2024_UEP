bcf5e03dc206a654e3134f5249d4fdc5748630f6
kamilvmichalak <kamilvmichalak@gmail.com>
2024-07-08 02:12:22 +0200

    dodatkowe testy w FirstAidTentTest

--- battle-model/src/test/java/pl/psi/creatures/FirstAidTentTest.java
+++ battle-model/src/test/java/pl/psi/creatures/FirstAidTentTest.java
@@ -3,6 +3,9 @@ package pl.psi.creatures;
 import com.google.common.collect.Range;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,4 +38,93 @@ class FirstAidTentTest {
         assertThat(dragon.getCurrentHp()).isEqualTo(dragon.getMaxHp());
     }
 
+    @Test
+    void firstAidTentShouldHeal() {
+        //given
+        final Creature angel = new Creature.Builder().statistic(CreatureStats.builder()
+                        .maxHp(100)
+                        .damage(Range.closed(10, 10))
+                        .attack(50)
+                        .armor(0)
+                        .build())
+                .build();
+        final Creature dragon = new Creature.Builder().statistic(CreatureStats.builder()
+                        .maxHp(100)
+                        .damage(NOT_IMPORTANT_DMG)
+                        .attack(0)
+                        .armor(10)
+                        .build())
+                .build();
+        MachineFactory machineFactory = new MachineFactory();
+        Creature firstAidTent = machineFactory.create("First Aid Tent");
+        // when
+        angel.attack(dragon);
+        firstAidTent.healHPCreature(dragon);
+        // then
+        assertThat(dragon.getCurrentHp()).isBetween(70,95);
+    }
+
+@Test
+    void firstAidTentShouldHealCreatureWithLowestHP() {
+        //given
+        final Creature angel = new Creature.Builder().statistic(CreatureStats.builder()
+                        .maxHp(100)
+                        .damage(Range.closed(10, 10))
+                        .attack(50)
+                        .armor(0)
+                        .build())
+                .build();
+        final Creature dragon = new Creature.Builder().statistic(CreatureStats.builder()
+                        .maxHp(100)
+                        .damage(NOT_IMPORTANT_DMG)
+                        .attack(0)
+                        .armor(10)
+                        .build())
+                .build();
+    final Creature zombie = new Creature.Builder().statistic(CreatureStats.builder()
+                    .maxHp(100)
+                    .damage(NOT_IMPORTANT_DMG)
+                    .attack(0)
+                    .armor(10)
+                    .build())
+            .build();
+    final Creature zombie2 = new Creature.Builder().statistic(CreatureStats.builder()
+                    .maxHp(100)
+                    .damage(NOT_IMPORTANT_DMG)
+                    .attack(0)
+                    .armor(10)
+                    .build())
+            .build();
+        MachineFactory machineFactory = new MachineFactory();
+        Creature firstAidTent = machineFactory.create("First Aid Tent");
+        List<Creature> creatureList = new ArrayList<>();
+        creatureList.add(dragon);
+        creatureList.add(zombie);
+        creatureList.add(zombie2);
+        // when
+        angel.attack(dragon);
+        dragon.attack(zombie);
+        dragon.attack(zombie2);
+        firstAidTent.chooseHealCreature(creatureList);
+        // then
+        assertThat(dragon.getCurrentHp()).isBetween(70,95);
+    }
+
+    @Test
+    void creatureWithFullHpShouldNotHaveMoreHPAfterHealing() {
+        //given
+        final Creature dragon = new Creature.Builder().statistic(CreatureStats.builder()
+                        .maxHp(100)
+                        .damage(NOT_IMPORTANT_DMG)
+                        .attack(0)
+                        .armor(10)
+                        .build())
+                .build();
+        MachineFactory machineFactory = new MachineFactory();
+        Creature firstAidTent = machineFactory.create("First Aid Tent");
+        // when
+        firstAidTent.healHPCreature(dragon);
+        // then
+        assertThat(dragon.getCurrentHp()).isEqualTo(dragon.getMaxHp());
+    }
 }
